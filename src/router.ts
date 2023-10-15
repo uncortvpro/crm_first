c972bd78f2279a0e5b7e37a69a7c62ca0f27ad5f
Roman <romazelenskiy2000@gmail.com>
2023-10-15 09:21:40 +0300

    fix router

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
